1ac2551892bed5b1b1016b2c767fc61eb3862ce8
e <fanghuav@126.com>
2023-05-31 21:33:54 +0800

    【update】注释部分代码

--- src/main/java/com/gupao/shotlink/service/Impl/MsmServiceImpl.java
+++ src/main/java/com/gupao/shotlink/service/Impl/MsmServiceImpl.java
@@ -22,7 +22,7 @@ public class MsmServiceImpl implements MsmService {
         if(StringUtils.isEmpty(phone)) return false;
 
         //default 地域节点，默认就好  后面是 阿里云的 id和秘钥（这里记得去阿里云复制自己的id和秘钥哦）
-        DefaultProfile profile = DefaultProfile.getProfile("default", "LTAI5tGotsQmNbpGowhr8hpc", "ju86uf4ky1xkXac9abXPUiyNSY1ngn");
+        DefaultProfile profile = DefaultProfile.getProfile("default", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         //这里不能修改
