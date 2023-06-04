04e275c27411fda861858cddc856434023a1a2a9
e <fanghuav@126.com>
2023-06-04 11:40:41 +0800

    【update】短链查询逻辑调整

--- src/main/java/com/gupao/shotlink/controller/UrlLinkController.java
+++ src/main/java/com/gupao/shotlink/controller/UrlLinkController.java
@@ -1,10 +1,15 @@
 package com.gupao.shotlink.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.gupao.shotlink.common.utils.RedisService;
+import com.gupao.shotlink.repository.UrlMapping;
+import com.gupao.shotlink.repository.mapper.UrlMappingMapper;
 import com.gupao.shotlink.service.Impl.UrlRedictServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +25,8 @@ public class UrlLinkController {
 
     @Autowired
     private RedisService redisService;
+    @Autowired
+    private UrlMappingMapper mapper;
 
     private Map<String, String> urlMap = new HashMap<>(); // 存储短链接和长链接的映射关系
 
@@ -31,6 +38,13 @@ public class UrlLinkController {
     @GetMapping("/api/redirect")
     public void redirectUrl(@RequestParam("url") String shortUrl, HttpServletResponse response) throws IOException {
         String longUrl = redisService.getString(shortUrl);
+        //redis中没有去数据库查询
+        if(StringUtils.isEmpty(longUrl)){
+            UrlMapping urlMapping = mapper.selectOne(new LambdaQueryWrapper<UrlMapping>().eq(UrlMapping::getShortUrl, shortUrl));
+            if(!ObjectUtils.isEmpty(urlMapping)){
+                longUrl = urlMapping.getOriginalUrl();
+            }
+        }
         response.setHeader("Prama","No-cache");
         response.setHeader("Cache-Control","no-cache");
         response.setHeader("refresh","0;URL="+longUrl);
